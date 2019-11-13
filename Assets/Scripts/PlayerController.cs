d4c0207a19d0ee586170fa69fb873c2f46f979b8
Lauren <lauren@wnkinc.co>
2019-11-13 13:17:43 -0800

    Created PlayerController script; added joystick functionality to player controls (PC only)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public Joystick joystick;
+  public float Speed = 3;
+  private Rigidbody rb;
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void FixedUpdate()
+  {
+		rb.velocity = new Vector3(joystick.Horizontal * 5f,
+      rb.velocity.y,
+      joystick.Vertical * 5f);
+    
+    if (Input.GetMouseButton(0))
+    {
+      transform.eulerAngles = new Vector3(transform.eulerAngles.x, 
+        Mathf.Atan2(joystick.Horizontal, joystick.Vertical) * Mathf.Rad2Deg, 
+        transform.eulerAngles.z);
+    }
+  }
+}
