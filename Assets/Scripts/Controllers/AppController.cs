a2340aa0f65039c2ee33be54ebe2af11b4052594
Lauren <lauren@wnkinc.co>
2019-11-23 17:01:51 -0800

    Updated app and game controllers

--- Assets/Scripts/Controllers/AppController.cs
+++ Assets/Scripts/Controllers/AppController.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AppController : Singleton<AppController> // Make this a gameobject singleton
+public class AppController : MonoBehaviour // Make this a gameobject singleton
 {
+#region Singleton Pattern
+  private static AppController _instance;
+  public static AppController Instance => _instance;
+
+  private void Awake()
+  {
+    if (_instance == null) _instance = this;
+    else Destroy(gameObject);
+
+    DontDestroyOnLoad(gameObject);
+  }
+#endregion
 
   public void OpenScene(string scene)
   {
