fa6df05feb51b0411210f12039309f1117bb2ee4
Lauren <lauren@wnkinc.co>
2019-12-14 17:27:46 -0800

    Setup for wave-spawning

---
+++ Assets/Scripts/WaveSystem/EnemyWaveManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaveManager : WaveManager<EnemyWave>
+{
+  public override IEnumerator SpawnWave()
+  {
+    yield return new WaitForSeconds(0);
+  }
+}
