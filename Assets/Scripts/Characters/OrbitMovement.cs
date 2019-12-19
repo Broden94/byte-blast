3db9762a6d874a0e5c7f83c6faccd197fa71ddc9
Lauren <lauren@wnkinc.co>
2019-12-19 11:38:30 -0800

    Meta file for Interfaces folder; implemented _canMove to derived classes

--- Assets/Scripts/Characters/OrbitMovement.cs
+++ Assets/Scripts/Characters/OrbitMovement.cs
@@ -2,7 +2,10 @@ public class OrbitMovement : EnemyMovement
 {
   public override void FixedUpdate()
   {
-    base.FixedUpdate();
-    Orbit(_centerOrbitTransform, _orbitSpeed);
+    if (_canMove)
+    {
+      base.FixedUpdate();
+      Orbit(_centerOrbitTransform, _orbitSpeed);
+    }
   }
 }
