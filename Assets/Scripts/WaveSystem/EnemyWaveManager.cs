db302153a56031b3b301149dbbd8ac420414f7a4
Lauren <lauren@wnkinc.co>
2019-12-18 16:40:31 -0800

    Implemented abstract classes from WaveManager base class; changed input for spawning waves

--- Assets/Scripts/WaveSystem/EnemyWaveManager.cs
+++ Assets/Scripts/WaveSystem/EnemyWaveManager.cs
@@ -6,13 +6,25 @@ public class EnemyWaveManager : WaveManager<EnemyWave>
 {
   private void Update()
   {
-    if (Input.GetKeyDown(KeyCode.S)) StartCoroutine(SpawnWave());
+    if (Input.GetKeyDown(KeyCode.Alpha1)) SpawnWaveElements();
+    else if (Input.GetKeyDown(KeyCode.Alpha2)) StartCoroutine(SpawnWaveElementsInOrder());
 
     // $LL - This works. Needs time buffer between waves. May need to convert List to Queue, but will face problem with manually inserting Waves.
-    if (EnemyManager.EnemyCount == 0) StartCoroutine(SpawnWave());
+    //if (EnemyManager.EnemyCount == 0) StartCoroutine(SpawnWaveElementsInOrder());
   }
 
-  public override IEnumerator SpawnWave()
+  public override void SpawnWaveElements()
+  {
+    for (int i = 0; i < Waves[_currentWaveIndex].WaveElements.Count; i++)
+    {
+      GameObject obj = Waves[_currentWaveIndex].WaveElements[i].NextPoolObject();
+      obj.transform.position = Waves[_currentWaveIndex].Spawnpoints[i].transform.position;
+      obj.transform.rotation = Quaternion.identity;
+      obj.SetActive(true);
+    }
+  }
+
+  public override IEnumerator SpawnWaveElementsInOrder()
   {
     for (int i = 0; i < Waves[_currentWaveIndex].WaveElements.Count; i++)
     {
